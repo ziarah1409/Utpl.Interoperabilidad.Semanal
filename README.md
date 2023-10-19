b3ec7b571273525c53ba7ceeeace5f7394444ace
fdquinones <fdquinones@gmail.com>
2023-10-19 17:13:32 -0500

    ajustes para proyecto de taller con gitpod

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Utpl.Interoperabilidad.Semanal
 Repositorio paara trabajar con Python
+
+
+
+Interoperabilidad
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/fdquinones/Utpl.Interoperabilidad.Semanal.git)
---
+++ main.py
@@ -0,0 +1,29 @@
+import paho.mqtt.client as mqtt
+
+# Definir los datos de conexión
+broker_address = "broker.hivemq.com"
+port = 1883  # Puerto predeterminado para MQTT
+topic = "mi_evento"  # Cambia "mi_evento" al nombre de tu evento
+
+# Función que se ejecuta cuando se conecta al broker
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Conectado al broker MQTT")
+        client.subscribe(topic)
+    else:
+        print(f"Error al conectarse, código de retorno: {rc}")
+
+# Función que se ejecuta cuando se recibe un mensaje
+def on_message(client, userdata, message):
+    print(f"Mensaje recibido en el tema '{message.topic}': {message.payload.decode()}")
+
+# Configurar el cliente MQTT
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+# Conectarse al broker
+client.connect(broker_address, port)
+
+# Mantener el cliente en funcionamiento
+client.loop_forever()
